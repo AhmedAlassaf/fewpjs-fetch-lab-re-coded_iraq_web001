11c303675bf9d9e942bcec8306a7fe0dae4703ef
Ahmed Alassaf <ahmedfaysal199@gmail.com>
2020-05-31 19:06:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 function fetchBooks() {
-  return fetch('https://anapioficeandfire.com/api/books').then(resp => resp.json()).then(json => console.log(json));
+  fetch('https://anapioficeandfire.com/api/books').then(resp => resp.json()).then(json => console.log(json));
+  renderBooks
 }
 
 function renderBooks(json) {
